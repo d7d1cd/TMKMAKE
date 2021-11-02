f8ad8bfc6585686b5353828d667602d75ab348ae
d7d1cd <d7d1cd@gmail.com>
2021-11-02 15:58:07 +0300

    EXCEPT Рефакторинг

--- TMKMAKE/MSGHANDLE$.C
+++ TMKMAKE/MSGHANDLE$.C
@@ -1,15 +1,15 @@
-/* ================================================================= */
-/*                                                                   */
-/*  MODULE:    TMKMSGHD                                              */
-/*                                                                   */
-/*                                                                   */
-/*  MODULE-TYPE: C Library                                           */
-/*                                                                   */
-/*  Processor:  C                                                    */
-/*                                                                   */
-/*  Purpose:    Message handling module                              */
-/*                                                                   */
-/* ================================================================= */
+// =================================================================
+//
+//  MODULE:    TMKMSGHD
+//
+//
+//  MODULE-TYPE: C Library
+//
+//  Processor:  C
+//
+//  Purpose:    Message handling module
+//
+// =================================================================
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,37 +19,37 @@
 #include "msghandle"
 #include "sysapi"
 
-/*****************************************************************************
-        Variables definitions
-*****************************************************************************/
 
+// =================================================================
+//  Variables definitions
+// =================================================================
 Static Boolean usrmsg_to_session = FALSE;
 Static error_rtn errrtn = {0};
 Static Char msg_key[4];
 Static Char msg_txt[MSG_TXT_SZ + MSG_LINE_NO_SZ];
 
-/* ================================================================= */
-/*  Function:    set_usrmsg_to_session ()                            */
-/* ================================================================= */
 
+// =================================================================
+//  Function:    set_usrmsg_to_session ()
+// =================================================================
 Void set_usrmsg_to_session(Void)
 {
   usrmsg_to_session = TRUE;
 }
 
-/* ================================================================= */
-/*  Function:    get_usrmsg_to_session ()                            */
-/* ================================================================= */
 
+// =================================================================
+//  Function:    get_usrmsg_to_session ()
+// =================================================================
 Boolean get_usrmsg_to_session(Void)
 {
   return (usrmsg_to_session);
 }
 
-/* ================================================================= */
-/*  Function:    log_error ()                                        */
-/* ================================================================= */
 
+// =================================================================
+//  Function:    log_error ()
+// =================================================================
 Void log_error(Char *msgid, Char *txt, Int32 line_no)
 {
   Char *msg = msg_txt;
@@ -58,7 +58,7 @@ Void log_error(Char *msgid, Char *txt, Int32 line_no)
 
   if (line_no > 0)
   {
-    /* setup the &1 if the message has line no information   */
+    // setup the &1 if the message has line no information
     sprintf(msg_txt, "%5d", line_no);
     msg = msg_txt + 5;
     msg_sz = 5;
@@ -66,7 +66,7 @@ Void log_error(Char *msgid, Char *txt, Int32 line_no)
   tmp_sz = (txt == NULL) ? 0 : strlen(txt);
   if (tmp_sz != 0)
   {
-    /* setup the rest of &2, &3.... parameters               */
+    // setup the rest of &2, &3.... parameters
     tmp_sz = Min(MSG_TXT_SZ, tmp_sz);
     memcpy(msg, txt, tmp_sz);
     msg_sz += tmp_sz;
@@ -76,10 +76,10 @@ Void log_error(Char *msgid, Char *txt, Int32 line_no)
            "*         ", 0, msg_key, (char *)&errrtn);
 }
 
-/* ================================================================= */
-/*  Function:    log_usrmsg ()                                       */
-/* ================================================================= */
 
+// =================================================================
+//  Function:    log_usrmsg ()
+// =================================================================
 Void log_usrmsg(Char *txt)
 {
   if (usrmsg_to_session)
@@ -92,16 +92,12 @@ Void log_usrmsg(Char *txt)
   }
 }
 
-/* ================================================================= */
-/*  Function:    log_dbg ()                                          */
-/* ================================================================= */
 
+// =================================================================
+//  Function:    log_dbg ()
+// =================================================================
 Void log_dbg(Char *txt)
 {
   fprintf(stdout, "%s\n", txt);
 }
-/*
-int main( void ) {
-        log_error ( "TMK9005", "This is more text", 1 );
-}
-*/
+
